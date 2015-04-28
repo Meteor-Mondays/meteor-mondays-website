6d8d2deb9dd875b7e090e965be6db9440b6434fc
Rob Fisher <me@robfisher.us>
2015-04-28 10:58:11 -0400

    Cleaned up some stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,21 @@
 'use strict';
 
+function getDate(month) {
+  var result = month.endOf('month');
+  while(result.day() !== 1)
+    result.subtract(1, 'day');
+
+  return result;
+}
+
 $(function() {
-  var dur = -1;
-  var result;
-  var month = moment();
+  var result = getDate(moment());
+  var dur = result.diff(moment());
 
-  do {
-    result = month.endOf('month');
-    while(result.day() !== 1) {
-      result.subtract(1, 'day');
-    }
-
-    dur = result.diff(moment());
-    month.add(1, 'month');
-  } while (dur < 0);
+  if (dur < 0)
+    result = getDate(moment().add(1, 'month'));
 
   var meeting = result.subtract(5.5, 'hours');
-  meeting.subtract(1, 'month');
   meeting.add(1, 'second');
 
   $('.next').text(meeting.format('dddd, MMMM Do YYYY, h:mm:ss a') + " EST");
