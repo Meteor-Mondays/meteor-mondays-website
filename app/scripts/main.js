ea714428c63374f8de239c2c6a8e206d0aa26561
Rob Fisher <me@robfisher.us>
2015-04-28 10:47:22 -0400

    Fixed for next meeting in same month

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,22 @@
 'use strict';
 
 $(function() {
-  var result = moment().endOf('month');
-  while(result.day() !== 1) {
-    result.subtract(1, 'day');
-  }
+  var dur = -1;
+  var result;
+  var month = moment();
+
+  do {
+    result = month.endOf('month');
+    while(result.day() !== 1) {
+      result.subtract(1, 'day');
+    }
+
+    dur = result.diff(moment());
+    month.add(1, 'month');
+  } while (dur < 0);
 
   var meeting = result.subtract(5.5, 'hours');
+  meeting.subtract(1, 'month');
   meeting.add(1, 'second');
 
   $('.next').text(meeting.format('dddd, MMMM Do YYYY, h:mm:ss a') + " EST");
